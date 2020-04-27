f6fa4a334e6396a9544f80d5108d005a5a69cb28
Nicky Singh <snicky@amazon.com>
2020-04-26 23:03:35 -0700

    Updated implementation of all handler

--- aws-ssm-maintenancewindow/src/test/java/software/amazon/ssm/maintenancewindow/CreateHandlerTest.java
+++ aws-ssm-maintenancewindow/src/test/java/software/amazon/ssm/maintenancewindow/CreateHandlerTest.java
@@ -1,5 +1,6 @@
 package software.amazon.ssm.maintenancewindow;
 
+<<<<<<< HEAD
 import software.amazon.cloudformation.proxy.AmazonWebServicesClientProxy;
 import software.amazon.cloudformation.proxy.Logger;
 import software.amazon.cloudformation.proxy.OperationStatus;
@@ -13,20 +14,88 @@ import org.mockito.junit.jupiter.MockitoExtension;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.Mockito.mock;
+=======
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.mockito.ArgumentMatchers;
+import software.amazon.awssdk.services.ssm.model.CreateMaintenanceWindowRequest;
+import software.amazon.awssdk.services.ssm.model.CreateMaintenanceWindowResponse;
+import software.amazon.awssdk.services.ssm.model.InternalServerErrorException;
+import software.amazon.cloudformation.exceptions.CfnServiceInternalErrorException;
+import software.amazon.cloudformation.proxy.AmazonWebServicesClientProxy;
+import software.amazon.cloudformation.proxy.Logger;
+import software.amazon.cloudformation.proxy.ResourceHandlerRequest;
+import software.amazon.cloudformation.proxy.ProgressEvent;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import software.amazon.ssm.maintenancewindow.translator.request.CreateMaintenanceWindowTranslator;
+import software.amazon.ssm.maintenancewindow.translator.ExceptionTranslator;
+
+import java.util.function.Function;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.ArgumentMatchers.eq;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.verifyZeroInteractions;
+>>>>>>> Updated implementation of all handler
 
 @ExtendWith(MockitoExtension.class)
 public class CreateHandlerTest {
 
+<<<<<<< HEAD
+=======
+    private static final Boolean ALLOWED_UNASSOCIATED_TARGETS = false;
+    private static final Integer CUTOFF = 1;
+    private static final String SCHEDULE = "cron(0 4 ? * SUN *)";
+    private static final Integer DURATION = 2;
+    private static final String NAME = "TestMaintenanceWindow";
+    private static final String WINDOW_ID = "mw-1234567890";
+    private static final ResourceModel model = ResourceModel.builder()
+            .name(NAME)
+            .allowUnassociatedTargets(ALLOWED_UNASSOCIATED_TARGETS)
+            .schedule(SCHEDULE)
+            .duration(DURATION)
+            .cutoff(CUTOFF)
+            .build();
+    private static final ResourceHandlerRequest<ResourceModel> request = ResourceHandlerRequest.<ResourceModel>builder()
+            .desiredResourceState(model)
+            .build();
+    private static final CreateMaintenanceWindowRequest createMaintenanceWindowRequest =
+            CreateMaintenanceWindowRequest.builder()
+                    .name(model.getName())
+                    .allowUnassociatedTargets(model.getAllowUnassociatedTargets())
+                    .schedule(model.getSchedule())
+                    .duration(model.getDuration())
+                    .cutoff(model.getCutoff())
+                    .build();
+
+    private CreateHandler handler;
+
+>>>>>>> Updated implementation of all handler
     @Mock
     private AmazonWebServicesClientProxy proxy;
 
     @Mock
     private Logger logger;
 
+<<<<<<< HEAD
+=======
+    @Mock
+    private ExceptionTranslator exceptionTranslator;
+
+    @Mock
+    private CreateMaintenanceWindowTranslator createMaintenanceWindowTranslator;
+
+>>>>>>> Updated implementation of all handler
     @BeforeEach
     public void setup() {
         proxy = mock(AmazonWebServicesClientProxy.class);
         logger = mock(Logger.class);
+<<<<<<< HEAD
     }
 
     @Test
@@ -50,5 +119,67 @@ public class CreateHandlerTest {
         assertThat(response.getResourceModels()).isNull();
         assertThat(response.getMessage()).isNull();
         assertThat(response.getErrorCode()).isNull();
+=======
+        exceptionTranslator = mock(ExceptionTranslator.class);
+        createMaintenanceWindowTranslator = mock(CreateMaintenanceWindowTranslator.class);
+        handler = new CreateHandler(createMaintenanceWindowTranslator, exceptionTranslator);
+    }
+
+    @Test
+    public void handleCreateRequestForSuccess() {
+
+        when(createMaintenanceWindowTranslator.resourceModelToRequest(model))
+                .thenReturn(createMaintenanceWindowRequest);
+
+        final CreateMaintenanceWindowResponse result =
+                CreateMaintenanceWindowResponse.builder()
+                        .windowId(WINDOW_ID)
+                        .build();
+
+        when(
+                proxy.injectCredentialsAndInvokeV2(
+                        eq(createMaintenanceWindowRequest),
+                        ArgumentMatchers.<Function<CreateMaintenanceWindowRequest, CreateMaintenanceWindowResponse>>any()))
+                .thenReturn(result);
+
+        final ResourceModel expectedModel = request.getDesiredResourceState();
+        expectedModel.setWindowId(WINDOW_ID);
+
+        final ProgressEvent<ResourceModel, CallbackContext> response
+                = handler.handleRequest(proxy, request, null, logger);
+
+        final ProgressEvent<ResourceModel, CallbackContext> expectedProgressEvent =
+                ProgressEvent.defaultSuccessHandler(expectedModel);
+
+        assertThat(response).isEqualTo(expectedProgressEvent);
+        verifyZeroInteractions(exceptionTranslator);
+
+    }
+
+    @Test
+    void handleCreateRequestThrowsTranslatedServiceException() {
+
+        when(createMaintenanceWindowTranslator.resourceModelToRequest(model))
+                .thenReturn(createMaintenanceWindowRequest);
+
+        final InternalServerErrorException serviceException = InternalServerErrorException.builder().build();
+
+        when(
+                proxy.injectCredentialsAndInvokeV2(
+                        eq(createMaintenanceWindowRequest),
+                        ArgumentMatchers.<Function<CreateMaintenanceWindowRequest, CreateMaintenanceWindowResponse>>any()))
+                .thenThrow(serviceException);
+
+        when(exceptionTranslator.translateFromServiceException(serviceException, createMaintenanceWindowRequest))
+                .thenReturn(new CfnServiceInternalErrorException("CreateMaintenanceWindowAssociation", serviceException));
+
+        Assertions.assertThrows(CfnServiceInternalErrorException.class, () -> {
+            handler.handleRequest(proxy, request, null, logger);
+        });
+
+        verify(exceptionTranslator)
+                .translateFromServiceException(serviceException, createMaintenanceWindowRequest);
+
+>>>>>>> Updated implementation of all handler
     }
 }
--- aws-ssm-maintenancewindow/src/test/java/software/amazon/ssm/maintenancewindow/DeleteHandlerTest.java
+++ aws-ssm-maintenancewindow/src/test/java/software/amazon/ssm/maintenancewindow/DeleteHandlerTest.java
@@ -1,32 +1,73 @@
 package software.amazon.ssm.maintenancewindow;
 
+<<<<<<< HEAD
 import software.amazon.cloudformation.proxy.AmazonWebServicesClientProxy;
 import software.amazon.cloudformation.proxy.Logger;
 import software.amazon.cloudformation.proxy.OperationStatus;
 import software.amazon.cloudformation.proxy.ProgressEvent;
 import software.amazon.cloudformation.proxy.ResourceHandlerRequest;
+=======
+import org.junit.jupiter.api.Assertions;
+>>>>>>> Updated implementation of all handler
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+<<<<<<< HEAD
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.Mockito.mock;
+=======
+import software.amazon.ssm.maintenancewindow.translator.ExceptionTranslator;
+import software.amazon.awssdk.services.ssm.model.TooManyUpdatesException;
+import software.amazon.awssdk.services.ssm.model.DeleteMaintenanceWindowRequest;
+import software.amazon.awssdk.services.ssm.model.DeleteMaintenanceWindowResponse;
+import software.amazon.cloudformation.exceptions.CfnThrottlingException;
+import software.amazon.cloudformation.proxy.AmazonWebServicesClientProxy;
+import software.amazon.cloudformation.proxy.Logger;
+import software.amazon.cloudformation.proxy.ResourceHandlerRequest;
+import software.amazon.cloudformation.proxy.ProgressEvent;
+import software.amazon.cloudformation.proxy.OperationStatus;
+import software.amazon.cloudformation.proxy.HandlerErrorCode;
+
+import java.util.function.Function;
+
+import org.mockito.ArgumentMatchers;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.ArgumentMatchers.eq;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.verifyZeroInteractions;
+import static org.mockito.Mockito.when;
+>>>>>>> Updated implementation of all handler
 
 @ExtendWith(MockitoExtension.class)
 public class DeleteHandlerTest {
 
+<<<<<<< HEAD
+=======
+    private static final String WINDOW_ID = "mw-1234567890";
+
+    private DeleteHandler handler;
+>>>>>>> Updated implementation of all handler
     @Mock
     private AmazonWebServicesClientProxy proxy;
 
     @Mock
     private Logger logger;
 
+<<<<<<< HEAD
+=======
+    @Mock
+    private ExceptionTranslator exceptionTranslator;
+
+>>>>>>> Updated implementation of all handler
     @BeforeEach
     public void setup() {
         proxy = mock(AmazonWebServicesClientProxy.class);
         logger = mock(Logger.class);
+<<<<<<< HEAD
     }
 
     @Test
@@ -51,4 +92,99 @@ public class DeleteHandlerTest {
         assertThat(response.getMessage()).isNull();
         assertThat(response.getErrorCode()).isNull();
     }
+=======
+        exceptionTranslator = mock(ExceptionTranslator.class);
+        handler = new DeleteHandler(exceptionTranslator);
+    }
+
+    @Test
+    public void handleRequestWithWindowId() {
+        final ResourceModel model = ResourceModel.builder()
+                .windowId(WINDOW_ID)
+                .build();
+
+        final DeleteMaintenanceWindowRequest expectedDeleteMaintenanceWindowRequest =
+                DeleteMaintenanceWindowRequest.builder()
+                        .windowId(model.getWindowId())
+                        .build();
+
+        final ResourceHandlerRequest<ResourceModel> request = ResourceHandlerRequest.<ResourceModel>builder()
+                .desiredResourceState(model)
+                .build();
+
+        final ProgressEvent<ResourceModel, CallbackContext> response =
+                handler.handleRequest(proxy, request, null, logger);
+
+        final ProgressEvent<ResourceModel, CallbackContext> expectedProgressEvent =
+                ProgressEvent.defaultSuccessHandler(null);
+
+        assertThat(response).isEqualTo(expectedProgressEvent);
+
+        verify(proxy)
+                .injectCredentialsAndInvokeV2(eq(expectedDeleteMaintenanceWindowRequest), ArgumentMatchers.<Function<DeleteMaintenanceWindowRequest, DeleteMaintenanceWindowResponse>>any());
+        verifyZeroInteractions(exceptionTranslator);
+    }
+
+    @Test
+    void handleRequestWithNoRequiredParametersPresent() {
+        final ResourceModel model = ResourceModel.builder()
+                .build();
+
+        final ResourceHandlerRequest<ResourceModel> request = ResourceHandlerRequest.<ResourceModel>builder()
+                .desiredResourceState(model)
+                .build();
+
+        final ProgressEvent<ResourceModel, CallbackContext> response =
+                handler.handleRequest(proxy, request, null, logger);
+
+        final ProgressEvent<ResourceModel, CallbackContext> expectedProgressEvent =
+                ProgressEvent.<ResourceModel, CallbackContext>builder()
+                        .resourceModel(model)
+                        .status(OperationStatus.FAILED)
+                        .errorCode(HandlerErrorCode.InvalidRequest)
+                        .message("WindowId must be specified to delete a maintenance window.")
+                        .build();
+
+        assertThat(response).isEqualTo(expectedProgressEvent);
+        verifyZeroInteractions(proxy);
+        verifyZeroInteractions(exceptionTranslator);
+    }
+
+    @Test
+    void handleRequestThrowsTranslatedServiceException() {
+        final ResourceModel model = ResourceModel.builder()
+                .windowId(WINDOW_ID)
+                .build();
+
+        final DeleteMaintenanceWindowRequest expectedDeleteMaintenanceWindowRequest =
+                DeleteMaintenanceWindowRequest.builder()
+                        .windowId(model.getWindowId())
+                        .build();
+
+        final ResourceHandlerRequest<ResourceModel> request = ResourceHandlerRequest.<ResourceModel>builder()
+                .desiredResourceState(model)
+                .build();
+
+        final TooManyUpdatesException serviceException = TooManyUpdatesException.builder().build();
+
+        when(
+                proxy.injectCredentialsAndInvokeV2(
+                        eq(expectedDeleteMaintenanceWindowRequest),
+                        ArgumentMatchers.<Function<DeleteMaintenanceWindowRequest, DeleteMaintenanceWindowResponse>>any()))
+                .thenThrow(serviceException);
+        when(
+                exceptionTranslator.translateFromServiceException(
+                        serviceException,
+                        expectedDeleteMaintenanceWindowRequest))
+                .thenReturn(new CfnThrottlingException("DeleteMaintenanceWindow", serviceException));
+
+        Assertions.assertThrows(CfnThrottlingException.class, () -> {
+            handler.handleRequest(proxy, request, null, logger);
+        });
+
+        verify(exceptionTranslator)
+                .translateFromServiceException(serviceException, expectedDeleteMaintenanceWindowRequest);
+    }
+
+>>>>>>> Updated implementation of all handler
 }
